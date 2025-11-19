039891b2f42b0fa45f6af281b3af1d08a0ee5b22
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2025-11-19 15:36:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+# 10
+s = "qwertyuiop"
+print(s)
+
+t = s[0:11:2]
+print(t)
+
+
+# 11
+s = "GrokByxAI"
+print(s)
+
+t = s[0:4] + s[-3]
+print(t)
+
+# 12
+s = "9876543210"
+print(s)
+
+t = s[::3]
+print(t)
+
+# 13
+s = "MachineLern"
+print(s)
+
+t = s[:-3]
+print(t)
+
+
+# 14
+s = "PythonJavaScript"
+print(s)
+
+t = s[6:10]
+print(t)
+
+# 15
+s = "abcdefighj"
+print(s)
+
+t = s[:5] + s[-5:]
+print(t)
+
+# 16
+s = "OpenAIxGoogleAI"
+print(s)
+
+t = s.count('AI')
+print(f"AI lar soni: {t}")
+
+res = 'AI' * t
+print(res)
+
+# 17
+s = "1234567890abs"
+print(s)
+
+t = s[1:10]
+print(t)
+
+t2 = s[10:13]
+print(t2)
+
+# 18
+s = "PalindromCheck"
+print(s)
+
+t = s[::4]
+print(t)
+
+t2 = s[-4::]
+print(t2)
+
+
+# 19
+s = "SuperGrok4lsHare"
+print(s)
+
+t = s[5:10]
+print(t)
